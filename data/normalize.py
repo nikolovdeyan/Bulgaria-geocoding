c8ede00b4329fb937db8ef48c64d3010ee67bcf3
Deyan Nikolov <deyan.nikolov@gmail.com>
2019-04-28 03:41:17 +0300

    Identified and applied several missed changes including missing and extra polygons.
    + Add polygons in the settlements layer for the following entities:
      - ID: 21193, vill. Dinkovo, located in LAU VID33, Vidin Province
      - ID: 62668, vill, Ridovo, located in LAU KRZ16, Kardzhali Province
      - ID: 78241, vill, Tsatsarovtsi, located in LAU SML11, Smolyan Province
    
    + Remove polygons and apply Settlement-Grounds relationship to the following entities:
      - ID 52283, vill. Novoseltsi, merged with ID 10971
      - ID 68970, vill. Starozagorski bani, merged with ID 70202
    
    + Remove circular self-reference as Parent-Child for the following features:
      - ID 20599, vill. Debelets
      - ID 22410, vill. Dolni Koriten
      - ID 54821, vill. Oreh
      - ID 58342, vill. Prileptsi

--- data/normalize.py
+++ data/normalize.py
@@ -1,19 +1,21 @@
-
-
 def normalize_column(infile, outfile):
+    """
+    Create a many-to-one relationship table from a multi-record column string.
+    """
     with open(infile) as src, open(outfile, 'w') as dest:
-        curr_name = None
         curr_ekt = None
         for line in src:
-            curr_name = line.split(',')[0]
-            curr_ekt = line.split(',')[1]
-            children_ekts = line.split(',')[2].rstrip().split(' ')
-            for ch in children_ekts:
-                result = f'{curr_name};{curr_ekt};{ch}\n'
-                dest.write(result)
+            curr_ekt = line.split(',')[0]
+            # The parent always belongs to its respective grounds.
+            children_ids = line.split(',')[1].rstrip().split(' ')
+            for child_id in children_ids:
+                if not child_id:
+                    continue
+                else:
+                    child_rec = f'{curr_ekt};{child_id}\n'
+                    dest.write(child_rec)
 
 
-          
 if __name__ == '__main__':
     input_file = 'settlements_grounds_relationships.csv'
     output_file = 'fixed_relationships.csv'
